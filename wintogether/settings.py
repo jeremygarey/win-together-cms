17fad7171bcb96d19ae512d31e4a182c0d935630
jgarey <jeremy_garey@taylor.edu>
2023-01-12 12:56:13 -0500

    cors

--- wintogether/settings.py
+++ wintogether/settings.py
@@ -100,16 +100,24 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "https://erudite-spot-374002.uc.r.appspot.com",
 ]
 
 ROOT_URLCONF = "wintogether.urls"
